0dcfe49ac6f26cce3f97f2e33af31bf9851b3782
Gabriel Leandro <guilhermeolima93@gmail.com>
2022-03-24 16:09:34 -0300

    add page empresa + responsividade

--- empresa.php
+++ empresa.php
@@ -3,15 +3,38 @@
 
 <section class="empresa">
   <div class="container">
-    <div class="empresa-text">
-      <h1 class="empresa-text-title">
-        Sobre a Jetech
-      </h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus eius maiores, accusantium fuga perferendis quas fugit consequatur natus voluptatum maxime. Rem tempore beatae dolorum ea aliquam? Magni sint soluta natus?</p>
+    <div class="empresa-content">
+      <div class="empresa-content-text">
+        <h1 class="empresa-content-text-title">
+          Sobre a Jetech
+        </h1>
+        <p class="empresa-content-text-subject">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur reiciendis nulla error quasi perferendis minus nam dolor expedita. Quo quam molestias necessitatibus aliquid dolor quia modi vero qui veritatis obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa itaque voluptatem saepe libero nemo deserunt rem quisquam? Culpa reprehenderit rem corrupti vel veniam. Esse dolore id, sapiente eum magnam iure! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa itaque voluptatem saepe libero nemo deserunt rem quisquam? Culpa reprehenderit rem corrupti vel veniam. Esse dolore id, sapiente eum magnam iure!</p>
+      </div>
+      <div class="empresa-content-img">
+        <img src="images/empresa-img.png" alt="Imagem de mãos juntas">
+      </div>
     </div>
-    <div class="empresa-img bk" data-bk="images/empresa-bk.png"></div>
+    <div class="empresa-about">
+      <div class="empresa-about-card">
+        <img src="images/missao.png" alt="Logo Missão">
+        <h3 class="empresa-about-card-title">Missão</h3>
+        <p class="empresa-about-card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, velit provident? Beatae doloribus illo et, quae, in fugiat sit sed, dignissimos deleniti deserunt minus facilis voluptate quod magni porro natus.</p>
+      </div>
+      <div class="empresa-about-card">
+        <img src="images/eye.png" alt="Logo Visão">
+        <h3 class="empresa-about-card-title">Visão</h3>
+        <p class="empresa-about-card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, velit provident? Beatae doloribus illo et, quae, in fugiat sit sed, dignissimos deleniti deserunt minus facilis voluptate quod magni porro natus.</p>
+      </div>
+      <div class="empresa-about-card">
+        <img src="images/like.png" alt="Logo Valores">
+        <h3 class="empresa-about-card-title">Valores</h3>
+        <p class="empresa-about-card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, velit provident? Beatae doloribus illo et, quae, in fugiat sit sed, dignissimos deleniti deserunt minus facilis voluptate quod magni porro natus.</p>
+      </div>
+    </div>
   </div>
 </section>
+
+
 
 <?php include('footer.php'); ?>
 </body>
