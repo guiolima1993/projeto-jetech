e529b69c961837b6cfde621f3e53c0ed0a27b7fd
Gabriel Leandro <guilhermeolima93@gmail.com>
2022-03-24 13:32:17 -0300

    primeiro commit para clonagem

---
+++ empresa.php
@@ -0,0 +1,18 @@
+<?php $_home = true ?>
+<?php include('header.php'); ?>
+
+<section class="empresa">
+  <div class="container">
+    <div class="empresa-text">
+      <h1 class="empresa-text-title">
+        Sobre a Jetech
+      </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus eius maiores, accusantium fuga perferendis quas fugit consequatur natus voluptatum maxime. Rem tempore beatae dolorum ea aliquam? Magni sint soluta natus?</p>
+    </div>
+    <div class="empresa-img bk" data-bk="images/empresa-bk.png"></div>
+  </div>
+</section>
+
+<?php include('footer.php'); ?>
+</body>
+</html>
