506bc07fc34745f51f1c860c7062aae12a24d33a
Gabriel Leandro <guilhermeolima93@gmail.com>
2022-03-28 09:20:31 -0300

    pages finalizadas e responsivas

--- servicos.php
+++ servicos.php
@@ -16,10 +16,12 @@
         </div>
         <button class="btn"><a href="orcamento.php">Contrate agora</a></button>
       </div>
+    </div>
+    <div class="servicos-cards">
       <div class="servicos-cards-item">
         <div class="servicos-cards-item-content">
           <h1 class="servicos-cards-item-content-title">
-            Serviço 1
+            Serviço 2
           </h1>
           <p class="servicos-cards-item-content-text">orem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisis gravida mi, in tempor nisi imperdiet vulputate. Fusce sed mauris eget nisl posuere ullamcorper. Suspendisse ornare mi semper metus pretium, nec placerat ipsum tincidunt. Nullam feugiat dolor ac lorem fringilla, sit amet egestas libero iaculis. Etiam dapibus pulvinar quam, sed dictum magna porttitor vel. Vivamus ac laoreet mauris. Mauris sit amet suscipit lectus, vitae luctus est. Quisque nunc dui, tincidunt vitae dolor eu, semper aliquet purus. Morbi vitae nibh pellentesque, sollicitudin nunc ut, imperdiet elit. In elementum purus nec nibh aliquam sagittis. Curabitur nec porttitor lectus.</p>
         </div>
